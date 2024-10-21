67e73d7ded5a41f73bfda25a8663a3a4ea97d67a
firos <vpzfiroz@gmail.com>
2024-10-21 06:27:40 +0530

    mongodb env bug fix

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -5,7 +5,7 @@ import { removeUserCredential } from "../redux/slices/user_slice";
 import { removeServiceProviderCredential } from "../redux/slices/sp_slice";
 
 const Api = axios.create({
-  baseURL: "https://cozastore.store",
+  baseURL: "https://live-care.online",
   withCredentials: true,
 });
 
